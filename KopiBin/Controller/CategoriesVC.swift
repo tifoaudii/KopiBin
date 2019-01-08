aa3a9a9a39633a47851c59cbe6248b489fc6708c
tifoaudy <tifofilkomub@gmail.com>
2019-01-08 12:37:37 +0700

    initial commit

---
+++ KopiBin/Controller/CategoriesVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KopiBin
+//
+//  Created by Tifo Audi Alif Putra on 08/01/19.
+//  Copyright © 2019 BCC FILKOM. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
